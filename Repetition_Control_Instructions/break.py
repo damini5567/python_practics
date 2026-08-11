c0dab794255f3e8ec954be1de03d97f983fe1d24
damini5567 <daminigaikwad2001@gmail.com>
2026-08-12 03:29:10 +0530

    break statement

---
+++ Repetition_Control_Instructions/break.py
@@ -0,0 +1,7 @@
+# Break Statment
+# When it exeutes break statement,it terminates the loop immediately,and exeutes the next statements in the program.
+for i in range(10):
+    if i==5:
+        break # used to terminate from this condition or loop.
+    print(i) 
+print("Execution Completed")
