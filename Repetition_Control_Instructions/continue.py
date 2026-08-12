686bbfc75ee7ff70448d91c33f928c0aac36d493
damini5567 <daminigaikwad2001@gmail.com>
2026-08-12 21:04:45 +0530

    continue statement

---
+++ Repetition_Control_Instructions/continue.py
@@ -0,0 +1,7 @@
+#Continue Statement
+#used to skip current iteration and start the next iteration
+for i in range(10):
+    if i==5:
+        continue # used to skip current iteration for ex: i==5 they skip 5
+    print(i) 
+print("Execution Completed")
